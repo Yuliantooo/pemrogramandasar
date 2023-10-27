7f3619f7d43dccdef1ec705915b16d6a1d690d91
Yuliantooo <148882794+Yuliantooo@users.noreply.github.com>
2023-10-27 19:37:04 +0700

    Create latihan1_variabel.py

---
+++ latihan1_variabel.py
@@ -0,0 +1,52 @@
+print("(a+b)(a-c)")
+print("-----------------------")
+a = 8
+b = 5
+c = 3
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("Hasil dari (a+b)(a-c) adalah :",(a+b)*(a-c))
+
+
+
+print("(a)(b)/(a+c)")
+print("-----------------------")
+a = 10
+b = 9
+c = 8
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("Hasil dari (a)(b)/(a+c) adalah :",(a*b)/(a+c))
+
+
+
+print("Luas Trapesium")
+print("-----------------------")
+a = 10
+b = 8
+t = 6
+print("a:",a)
+print("b:",b)
+print("t:",t) 
+print("Rumus trapesium yaitu 1/2 (a+b) t")
+print("Jadi hasil dari luas trapesium adalah:",1/2*(a+b)*t) 
+
+
+
+print("Keliling Lingkaran")
+print("-----------------------")
+r = 14
+print("r:",r)
+print("Rumus keliling lingkaran yaitu 2πr")
+print("Hasil keliling lingkarannya adalah:",2*22/7*r)
+
+
+
+print("Luas Lingkaran")
+print("-----------------------")
+r = 10
+print("r:",r)
+print("Rumus luas lingkaran yaitu πr²")
+print("Hasil luas  lingkarannya adalah:",3.14*r*r)
