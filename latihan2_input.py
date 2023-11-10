d61b5b0ffdf46f128734273c6c18bd540fe96dbf
Yuliantooo <148882794+Yuliantooo@users.noreply.github.com>
2023-11-10 12:34:50 +0700

    Create latihan2_input.py

---
+++ latihan2_input.py
@@ -0,0 +1,20 @@
+r=int(input("Masukkan Jari Jari:"))
+luas = 3.14*r*r
+print("Hasil perhitungan luas lingkaran")
+print("==============================")
+print("r:",r)
+print ("luas lingkaran:", luas)
+print("==============================")
+
+
+nama = input("Masukkan Nama:")
+nim = input("Masukkan Nim:")
+kelas = input("Masukkan Kelas:")
+prodi = input("Masukkan Prodi:")
+print("=======================")
+print("biodata mahasiswa")
+print("=======================")
+print("Nama :",nama)
+print("NIM :",nim)
+print("Kelas :",kelas)
+print("Prodi :",prodi
